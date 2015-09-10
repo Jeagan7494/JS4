cd4fc1ab6fdd65f9fa950d4e628f9de1dc351b75
Student <student@RU-104.local>
2015-09-10 12:17:34 -0600

    mid Thursday

--- JS4.js
+++ JS4.js
@@ -1,3 +1,18 @@
-var howManyVictims = prompt("How many victims are in need?")
-var numOfVictims = howManyVictims
+var howManyVictims = prompt("Entered estimated number victims in need:")
+var victim = []
+var victimInfo = {
+
+};
+for ( i = 0; i < howManyVictims; i++) {
+	var victimName = prompt("Enter victim's first and last name: ")
+	var phoneNumber = prompt("Enter victim's phone number: ")
+	var address = prompt("Enter victim's address: ")
+
+	victim.push(howManyVictims);
+	victim.push(victimName);
+	victim.push(phoneNumber);
+	victim.push(address);
+
+	console.log(victim)
+};
 
